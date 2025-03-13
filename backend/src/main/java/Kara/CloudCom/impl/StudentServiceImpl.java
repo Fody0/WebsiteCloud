5fba51b112a562802328e11b0e630ecea43ceb91
Mogilwolf <carkarina1@gmail.com>
2025-03-13 13:30:06 +0300

    Uncheck

--- backend/src/main/java/Kara/CloudCom/impl/StudentServiceImpl.java
+++ backend/src/main/java/Kara/CloudCom/impl/StudentServiceImpl.java
@@ -14,32 +14,32 @@ import java.util.List;
 @AllArgsConstructor
 @Primary
 public class StudentServiceImpl implements StudentService {
-    private final StudentRepository rep;
+    private final StudentRepository repository;
 
     @Override
     public List<Student> findAllStudent() {
-        return rep.findAll();
+        return repository.findAll();
     }
 
     @Override
     public Student saveStudent(Student student) {
-        return rep.save(student);
+        return repository.save(student);
     }
 
     @Override
     public Student findByEmail(String email) {
-        return rep.findByEmail(email);
+        return repository.findByEmail(email);
     }
 
     @Override
     public Student updateStudent(Student student) {
-        return rep.save(student);
+        return repository.save(student);
     }
 
     @Override
     @Transactional
     public void deleteStudent(String email) {
-        rep.deleteByEmail(email);
+        repository.deleteByEmail(email);
 
     }
 }
