be47e05cbf611b88a1d864cf3e8fb5219ec0f4b9
Артем Носков <artyomnoskov15@gmail.com>
2025-03-27 15:39:26 +0300

    Session configured

---
+++ backend/src/src/main/java/Kara/CloudCom/Controller/UserController.java
@@ -0,0 +1,58 @@
+package Kara.CloudCom.Controller;
+
+import Kara.CloudCom.Model.Requests.LoginRequest;
+import Kara.CloudCom.Model.User;
+import Kara.CloudCom.Service.UserService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/Users")
+@AllArgsConstructor
+public class UserController {
+
+    private final UserService service_a;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @GetMapping()
+    public List<User> findAllUser() {
+        //TODO
+        return service_a.findAllUser();
+    }
+
+    @PostMapping("save_user")
+    @CrossOrigin(origins = "http://localhost:3000/",
+            methods = {RequestMethod.POST}, allowCredentials = "true")
+    public String saveUser(@RequestBody User user) {
+        // Хешируем пароль перед сохранением
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
+        service_a.saveUser(user);
+        return "user successfully saved";
+    }
+
+    @PostMapping("/login")
+    public User loginUser(@RequestBody LoginRequest email) {
+        User foundUser = service_a.findByEmail(email.getEmail());
+        if (foundUser != null && passwordEncoder.matches(email.getPassword(), foundUser.getPassword())) {
+            return foundUser;
+        } else {
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Invalid credentials");
+        }
+    }
+}
+
+//    @PutMapping("update_User")
+//    public User updateUser(User User) {
+//        return service_a.updateUser(User);
+//    }
+//    @DeleteMapping("delete_User/{email}")
+//    public void deleteUser(@PathVariable String email) {
+//        service_a.deleteUser(email);
+//    }
+//}
---
+++ backend/src/src/main/java/Kara/CloudCom/impl/InMemoryUserServiceImpl.java
@@ -0,0 +1,48 @@
+package Kara.CloudCom.impl;
+
+import Kara.CloudCom.Model.User;
+import Kara.CloudCom.Service.UserService;
+import Kara.CloudCom.repository.InMemoryUserDAO;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class InMemoryUserServiceImpl implements UserService {
+    private final InMemoryUserDAO repository;
+    @Override
+    public List<User> findAllUser() {
+        return repository.findAllUser();
+    }
+    @Override
+    public User saveUser(User User) {
+        return repository.saveUser(User);
+    }
+
+    @Override
+    public User findByEmail(String email) {
+        return repository.findByEmail(email);
+    }
+
+    @Override
+    public User confirmUserPassword(String password) {
+        return null;
+    }
+
+    @Override
+    public User updateUser(User User) {
+        return repository.updateUser(User);
+    }
+
+    @Override
+    public void deleteUser(String email) {
+        repository.deleteUser(email);
+    }
+
+    @Override
+    public boolean checkPassword(User foundUser, String password) {
+        return false;
+    }
+}
